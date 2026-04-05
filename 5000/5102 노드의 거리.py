53dcb649cd818650300dc48929377e72266fcd08
Ki-baek <woora03@naver.com>
2026-04-05 18:20:22 +0900

    commit 5102

---
+++ 5000/5102 노드의 거리.py
@@ -0,0 +1,50 @@
+from collections import deque
+
+def bfs(start, end):
+  if start == end:
+    return 0
+  
+  queue = deque([start])
+  cnt = 1
+  lst = []
+  judge = False
+  while queue:
+    
+    k = queue.popleft()
+    
+    if end in node[k]:
+      judge = True
+      break
+    
+    elif visited[k] == 0:
+      lst.extend(node[k])
+      visited[k] += 1
+
+    if not queue:
+      queue.extend(lst)
+      lst = []
+      cnt += 1
+  if judge:
+    return cnt
+  
+  else:
+    return 0
+
+T = int(input())
+
+for t in range(1, T + 1):
+  v, e = map(int, input().split())
+  
+  node = [[] for _ in range(v + 1)]
+  
+  for i in range(e):
+    a, b = map(int, input().split())
+    
+    node[a].append(b)
+    node[b].append(a)
+    
+  s, g = map(int, input().split())
+  
+  visited = [0] * (v + 1)
+  
+  print(f'#{t} {bfs(s, g)}')
