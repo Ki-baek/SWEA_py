fce8676d1097de45639c9a0e12014872a407762d
Ki-baek <woora03@naver.com>
2026-04-05 19:05:39 +0900

    commit 5105

---
+++ 5000/5105 미로의 거리.py
@@ -0,0 +1,48 @@
+def dfs(stx, sty):
+  global cnt
+  global result
+  
+  dx = [1, -1, 0, 0]
+  dy = [0, 0, -1, 1]
+  
+  
+
+  for i in range(4):
+
+    nowX = stx + dx[i]
+    nowY = sty + dy[i]
+    
+    if 0 <= nowX < n and 0 <= nowY < n:
+      if maze[nowY][nowX] == 0 and visited[nowY][nowX] == 0:
+        visited[nowY][nowX] += 1
+        cnt += 1
+        dfs(nowX, nowY)
+        cnt -= 1
+      elif maze[nowY][nowX] == 3:
+        result = cnt
+        return
+  
+
+  
+    
+
+
+T = int(input())
+
+for t in range(1, T + 1):
+  n = int(input())
+  result = 0
+  cnt = 0
+  maze = [list(map(int, input().rstrip())) for _ in range(n)]
+  
+  for y in range(n):
+    for x in range(n):
+      if maze[y][x] == 2:
+        startX, startY = x, y
+        
+  
+  visited = [[0 for i in range(n)] for _ in range(n)]
+    
+  dfs(startX, startY)
+  
+  print(f'#{t} {result}')
